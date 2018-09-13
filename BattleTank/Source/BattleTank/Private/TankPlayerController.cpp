6b897f1b990d6a845e290da9fa47ac32bc79c1f4
drevenb <drevenb@gmail.com>
2018-09-13 14:22:15 +0300

    BT13 Check PlayerController possessing a Tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,9 +2,23 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if(ControlledTank)
+	{
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing a %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+	UE_LOG(LogTemp, Error, TEXT("PlayerController not possessing a tank!"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
-
